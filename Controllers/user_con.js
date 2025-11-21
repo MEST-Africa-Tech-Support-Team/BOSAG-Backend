40e3b0c41b881e7955845017c5215860b4a41ec0
comfort-parker <comfortparker2001@outlook.com>
2025-11-21 14:59:49 +0000

    Added confirmed password to my joi validation

--- Controllers/user_con.js
+++ Controllers/user_con.js
@@ -12,6 +12,10 @@ const registerSchema = Joi.object({
   lastName: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
+  confirmPassword: Joi.string().valid(Joi.ref("password")).required()
+    .messages({
+      "any.only": "Passwords do not match",
+    }),
   provider: Joi.string().valid("google", "facebook", "email").default("email"),
 });
 
